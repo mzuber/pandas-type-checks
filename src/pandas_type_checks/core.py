8989ff7bce93d5ffa7526848cdd7683f0cc8468a
Martin Zuber <martin.zuber@sap.com>
2021-10-28 22:52:09 +0200

    Added decorator and implementation draft for type checks on data frames.

--- src/pandas_type_checks/core.py
+++ src/pandas_type_checks/core.py
@@ -1,9 +1,30 @@
-from typing import Dict, Any, Union
+from typing import Dict, Any, Union, Optional, List
 
 import pandas as pd
 import numpy as np
 
 from pandas.core.dtypes.base import ExtensionDtype
+
+
+class PandasTypeCheckError(object):
+    """
+    Error-related information when type checking a Pandas data frame or series.
+
+    Attributes:
+        error_msg: Error message
+        expected_type: Expected type for the data frame column or series
+        given_type: (Optional) Actual type of the data frame column or series
+        column_name: (Optional) Data frame column name, set if error occurred
+                     when type checking a column of a data frame
+    """
+
+    def __init__(self, error_msg: str, expected_type: Any,
+                 given_type: Optional[Any] = None,
+                 column_name: Optional[str] = None):
+        self.error_msg = error_msg
+        self.expected_type = expected_type
+        self.given_type = given_type
+        self.column_name = column_name
 
 
 class SeriesReturnValue(object):
@@ -17,7 +38,7 @@ class SeriesReturnValue(object):
     def __init__(self, dtype: Union[str, np.dtype, ExtensionDtype]):
         self.dtype = dtype
 
-    def type_check(self, series: pd.Series, strict: bool = False):
+    def type_check(self, series: pd.Series, strict: bool = False) -> List[PandasTypeCheckError]:
         pass
 
 
@@ -44,7 +65,7 @@ class DataFrameReturnValue(object):
             Expected data type for the DataFrame.
 
             Data type, or dict of column name -> data type
-            Use a single numpy.dtype or Python type to mark that all columns ih the DataFrame have the same type.
+            Use a single numpy.dtype or Python type to mark that all columns in the DataFrame have the same type.
             Alternatively, use {col: dtype, ...}, where 'col' is a column label and 'dtype' is a numpy.dtype or
             Python type to mark that one or more of the DataFrame's columns have the given column-specific types.
     """
@@ -52,8 +73,31 @@ class DataFrameReturnValue(object):
     def __init__(self, dtype: Dict[str, Any]):
         self.dtype = dtype
 
-    def type_check(self, data_frame: pd.DataFrame, strict: bool = False):
-        pass
+    def type_check(self, data_frame: pd.DataFrame, strict: bool = False) -> List[PandasTypeCheckError]:
+
+        type_check_errors: List[PandasTypeCheckError] = []
+        reference_data_frame = pd.DataFrame(columns=self.dtype.keys()).astype(self.dtype)
+
+        for column_name in self.dtype.keys():
+            expected_column_type = reference_data_frame[column_name].dtype
+
+            if column_name not in data_frame.columns:
+                type_check_error = PandasTypeCheckError(error_msg=f"Missing column in DataFrame: '{column_name}'",
+                                                        expected_type=expected_column_type,
+                                                        column_name=column_name)
+                type_check_errors.append(type_check_error)
+            else:
+                column_type = data_frame[column_name].dtype
+                if column_type != expected_column_type:
+                    error_msg = f"""Expected type '{expected_column_type}' for column  "
+                                    '{column_name}' but found type '{column_type}'"""
+                    type_check_error = PandasTypeCheckError(error_msg=error_msg,
+                                                            expected_type=expected_column_type,
+                                                            given_type=column_type,
+                                                            column_name=column_name)
+                    type_check_errors.append(type_check_error)
+
+        return type_check_errors
 
 
 class DataFrameArgument(DataFrameReturnValue):
@@ -67,7 +111,7 @@ class DataFrameArgument(DataFrameReturnValue):
             Expected data type for the DataFrame.
 
             Data type, or dict of column name -> data type
-            Use a single numpy.dtype or Python type to mark that all columns ih the DataFrame have the same type.
+            Use a single numpy.dtype or Python type to mark that all columns in the DataFrame have the same type.
             Alternatively, use {col: dtype, ...}, where 'col' is a column label and 'dtype' is a numpy.dtype or
             Python type to mark that one or more of the DataFrame's columns have the given column-specific types.
     """
--- src/pandas_type_checks/decorator.py
+++ src/pandas_type_checks/decorator.py
@@ -1,4 +1,16 @@
+import inspect
 from functools import wraps
+from typing import List, Dict
+
+import pandas as pd
+
+from pandas_type_checks.core import DataFrameArgument, DataFrameReturnValue, SeriesArgument, SeriesReturnValue
+from pandas_type_checks.core import PandasTypeCheckError
+
+
+class PandasTypeCheckDecoratorException(Exception):
+    """An exception class for errors related to the Pandas type check decorator."""
+    pass
 
 
 def pandas_type_check(*args, **kwargs):
@@ -7,7 +19,52 @@ def pandas_type_check(*args, **kwargs):
 
         @wraps(func)
         def pandas_type_check_wrapper(*func_args, **func_kwargs):
-            # TODO: type check data frame args
+            # Get argument specification of wrapped function
+            func_spec = inspect.getfullargspec(func)
+
+            # Evaluate query args of the decorator
+            strict: bool = kwargs.get('strict', False)
+
+            # Argument name -> type check errors found for given argument
+            type_check_errors: Dict[str, List[PandasTypeCheckError]] = {}
+
+            for arg in args:
+                if isinstance(arg, (DataFrameArgument, SeriesArgument)):
+                    # Check if wrapped function has an argument with the given name
+                    if arg.name in func_spec.args:
+                        # Check if argument of wrapped function is a DataFrame or Series
+                        func_arg = func_args[func_spec.args.index(arg.name)]
+                        if not isinstance(func_arg, (pd.DataFrame, pd.Series)):
+                            raise PandasTypeCheckDecoratorException(
+                                f"Argument type mismatch. Expected argument '{arg.name}' of decorated "
+                                f"function '{func.__name__}' to be a Pandas DataFrame or Series but "
+                                f"found value of type '{str(type(func_arg))}'."
+                            )
+
+                        # Compare DataFrame/Series structure of function argument
+                        # with the expected structure given in the type check marker
+                        arg_type_check_errors: List[PandasTypeCheckError] = arg.type_check(func_arg, strict=strict)
+                        if arg_type_check_errors:
+                            type_check_errors[arg.name] = arg_type_check_errors
+                    else:
+                        raise PandasTypeCheckDecoratorException(
+                            f"Decorated function '{func.__name__}' has no parameter '{arg.name}'."
+                        )
+                elif isinstance(arg, (DataFrameReturnValue, SeriesReturnValue)):
+                    pass
+                else:
+                    raise PandasTypeCheckDecoratorException(
+                        f"Unsupported argument for decorator. Expected argument of type "
+                        f"'DataFrameArgument', 'DataFrameReturnValue', 'SeriesArgument', or "
+                        f"'SeriesReturnValue' but found type '{str(type(arg))}'."
+                    )
+
+            # Raise type error if any type check errors were found for any of the Pandas arguments or return value
+            if type_check_errors:
+                # TODO: Build error message containing all collected type check errors
+                error_msg = str(type_check_errors)
+                raise TypeError(error_msg)
+
             return func(*func_args, **func_kwargs)
 
         return pandas_type_check_wrapper
