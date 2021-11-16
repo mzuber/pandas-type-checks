ca72d32d02dbb41b790fc186837eb019117d0ede
Martin Zuber <martin.zuber@sap.com>
2021-11-16 23:07:21 +0100

    Refactored type checking of Pandas DataFrame and Series arguments in decorator.

--- src/pandas_type_checks/decorator.py
+++ src/pandas_type_checks/decorator.py
@@ -1,6 +1,6 @@
 import inspect
 from functools import wraps
-from typing import List, Dict
+from typing import List, Dict, Union, Type
 
 import pandas as pd
 
@@ -28,56 +28,47 @@ def pandas_type_check(*args, **kwargs):
             # Argument name -> type check errors found for given argument
             type_check_errors: Dict[str, List[PandasTypeCheckError]] = {}
 
-            for arg in args:
-                if isinstance(arg, DataFrameArgument):
-                    # Check if wrapped function has an argument with the given name
-                    if arg.name in func_spec.args:
-                        # Check if argument of wrapped function is a DataFrame
-                        func_arg = func_args[func_spec.args.index(arg.name)]
-                        if not isinstance(func_arg, pd.DataFrame):
-                            raise PandasTypeCheckDecoratorException(
-                                f"Argument type mismatch. Expected argument '{arg.name}' of decorated "
-                                f"function '{func.__name__}' to be a Pandas DataFrame but found value "
-                                f"of type '{str(type(func_arg))}'."
-                            )
-
+            def check_pandas_arg(decorator_arg: Union[DataFrameArgument, SeriesArgument],
+                                 expected_function_arg_type: Union[Type[pd.DataFrame], Type[pd.Series]]
+                                 ) -> List[PandasTypeCheckError]:
+                """Type check Pandas DataFrame and Series arguments."""
+                # Check if wrapped function has an argument with the given name
+                if decorator_arg.name in func_spec.args:
+                    # Check if argument of wrapped function is a DataFrame
+                    func_arg = func_args[func_spec.args.index(decorator_arg.name)]
+                    if isinstance(func_arg, expected_function_arg_type):
                         # Compare DataFrame structure of function argument with
                         # the expected structure given in the type check marker
-                        arg_type_check_errors: List[PandasTypeCheckError] = arg.type_check(func_arg, strict=strict)
-                        if arg_type_check_errors:
-                            type_check_errors[arg.name] = arg_type_check_errors
+                        return decorator_arg.type_check(func_arg, strict=strict)
                     else:
                         raise PandasTypeCheckDecoratorException(
-                            f"Decorated function '{func.__name__}' has no parameter '{arg.name}'."
+                            f"Argument type mismatch. Expected argument '{decorator_arg.name}' of decorated function "
+                            f"'{func.__name__}' to be of type '{expected_function_arg_type.__qualname__}' but found "
+                            f"value of type '{type(func_arg).__qualname__}'."
                         )
-                elif isinstance(arg, SeriesArgument):
-                    # Check if wrapped function has an argument with the given name
-                    if arg.name in func_spec.args:
-                        # Check if argument of wrapped function is a Series
-                        func_arg = func_args[func_spec.args.index(arg.name)]
-                        if not isinstance(func_arg, pd.Series):
-                            raise PandasTypeCheckDecoratorException(
-                                f"Argument type mismatch. Expected argument '{arg.name}' of decorated "
-                                f"function '{func.__name__}' to be a Pandas Series but found value of "
-                                f"type '{str(type(func_arg))}'."
-                            )
+                else:
+                    raise PandasTypeCheckDecoratorException(
+                        f"Decorated function '{func.__name__}' has no parameter '{decorator_arg.name}'."
+                    )
 
-                        # Compare Series structure of function argument with
-                        # the expected structure given in the type check marker
-                        arg_type_check_errors: List[PandasTypeCheckError] = arg.type_check(func_arg, strict=strict)
-                        if arg_type_check_errors:
-                            type_check_errors[arg.name] = arg_type_check_errors
-                    else:
-                        raise PandasTypeCheckDecoratorException(
-                            f"Decorated function '{func.__name__}' has no parameter '{arg.name}'."
-                        )
+            # Perform type checks for Pandas arguments and return value defined in decorator
+            for arg in args:
+                if isinstance(arg, DataFrameArgument):
+                    arg_type_check_errors: List[PandasTypeCheckError] = check_pandas_arg(arg, pd.DataFrame)
+                    if arg_type_check_errors:
+                        type_check_errors[arg.name] = arg_type_check_errors
+                elif isinstance(arg, SeriesArgument):
+                    arg_type_check_errors: List[PandasTypeCheckError] = check_pandas_arg(arg, pd.Series)
+                    if arg_type_check_errors:
+                        type_check_errors[arg.name] = arg_type_check_errors
                 elif isinstance(arg, (DataFrameReturnValue, SeriesReturnValue)):
                     pass
                 else:
                     raise PandasTypeCheckDecoratorException(
                         f"Unsupported argument for decorator. Expected argument of type "
-                        f"'DataFrameArgument', 'DataFrameReturnValue', 'SeriesArgument', or "
-                        f"'SeriesReturnValue' but found type '{str(type(arg))}'."
+                        f"'{DataFrameArgument.__qualname__}', '{DataFrameReturnValue.__qualname__}', "
+                        f"'{SeriesArgument.__qualname__}', or '{SeriesReturnValue.__qualname__}' but "
+                        f"found type '{type(arg).__qualname__}'."
                     )
 
             # Raise type error if any type check errors were found for any of the Pandas arguments or return value
