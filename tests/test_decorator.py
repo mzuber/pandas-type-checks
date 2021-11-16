92a28151eff64541b726bb90b594fe2105572269
Martin Zuber <martin.zuber@sap.com>
2021-11-16 23:07:51 +0100

    Added more tests for error cases.

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,7 +1,8 @@
+import pytest
 import pandas as pd
 
 from pandas_type_checks.core import SeriesReturnValue, SeriesArgument, DataFrameReturnValue, DataFrameArgument
-from pandas_type_checks.decorator import pandas_type_check
+from pandas_type_checks.decorator import pandas_type_check, PandasTypeCheckDecoratorException
 
 
 def test_data_frame_argument(data_frame, data_frame_type):
@@ -42,3 +43,49 @@ def test_series_return_value(series, series_type):
 
     result = test_function()
     assert result.equals(series) is True
+
+
+def test_argument_type_mismatch(data_frame_type):
+
+    @pandas_type_check(DataFrameArgument('arg', data_frame_type))
+    def test_function(arg: str) -> str:
+        return arg
+
+    with pytest.raises(PandasTypeCheckDecoratorException,
+                       match="Argument type mismatch. Expected argument 'arg' of decorated function "
+                             "'test_function' to be of type 'DataFrame' but found value of type 'str'."):
+        test_function("string")
+
+
+def test_unknown_argument(data_frame_type):
+    # Decorated function has no arguments
+    @pandas_type_check(DataFrameArgument('arg', data_frame_type))
+    def test_function1() -> str:
+        return "string"
+
+    with pytest.raises(PandasTypeCheckDecoratorException,
+                       match=f"Decorated function '{test_function1.__name__}' has no parameter 'arg'."):
+        test_function1()
+
+    # Decorated function has arguments but not the one's defined in the decorator
+    @pandas_type_check(DataFrameArgument('arg', data_frame_type))
+    def test_function2(another_arg: str) -> str:
+        return another_arg
+
+    with pytest.raises(PandasTypeCheckDecoratorException,
+                       match=f"Decorated function '{test_function2.__name__}' has no parameter 'arg'."):
+        test_function2("string")
+
+
+def test_unsupported_decorator_argument():
+
+    @pandas_type_check("Unsupported String Argument")
+    def test_function() -> int:
+        return 0
+
+    with pytest.raises(PandasTypeCheckDecoratorException,
+                       match=f"Unsupported argument for decorator. Expected argument of type "
+                             f"'{DataFrameArgument.__qualname__}', '{DataFrameReturnValue.__qualname__}', "
+                             f"'{SeriesArgument.__qualname__}', or '{SeriesReturnValue.__qualname__}' but "
+                             f"found type 'str'."):
+        test_function()
